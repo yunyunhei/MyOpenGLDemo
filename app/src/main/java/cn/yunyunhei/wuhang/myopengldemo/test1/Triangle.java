4d8d8fff4e7e99c4f06cffe75d40fe1adbfa578e
yunyunhei <710617311@qq.com>
2017-04-22 18:12:59 +0800

    OpenGL的基础认识练习三，图形绘制，三角形

--- app/src/main/java/cn/yunyunhei/wuhang/myopengldemo/test1/Triangle.java
+++ app/src/main/java/cn/yunyunhei/wuhang/myopengldemo/test1/Triangle.java
@@ -1,5 +1,7 @@
 package cn.yunyunhei.wuhang.myopengldemo.test1;
 
+import android.opengl.GLES20;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
@@ -9,6 +11,19 @@ import java.nio.FloatBuffer;
  */
 
 public class Triangle {
+
+    private final String vertexShaderCode =
+            "attribute vec4 vPosition;" +
+                    "void main() {" +
+                    "  gl_Position = vPosition;" +
+                    "}";
+    private final String fragmentShaderCode =
+            "precision mediump float;" +
+                    "uniform vec4 vColor;" +
+                    "void main() {" +
+                    "  gl_FragColor = vColor;" +
+                    "}";
+
     private FloatBuffer vertexBuffer;
     // number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 3;
@@ -32,5 +47,52 @@ public class Triangle {
         vertexBuffer.put(triangleCoords);
         // set the buffer to read the first coordinate
         vertexBuffer.position(0);
+
+
+        int vertexShader = MyGLRenderer.loadShader(GLES20.GL_VERTEX_SHADER,
+                vertexShaderCode);
+        int fragmentShader = MyGLRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER,
+                fragmentShaderCode);
+
+        // create empty OpenGL ES Program
+        mProgram = GLES20.glCreateProgram();
+        // add the vertex shader to program
+        GLES20.glAttachShader(mProgram, vertexShader);
+        // add the fragment shader to program
+        GLES20.glAttachShader(mProgram, fragmentShader);
+        // creates OpenGL ES program executables
+        GLES20.glLinkProgram(mProgram);
     }
+
+    private final int mProgram;
+
+
+
+    private int mPositionHandle;
+    private int mColorHandle;
+    private final int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
+    private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
+
+    public void draw() {
+        // Add program to OpenGL ES environment
+        GLES20.glUseProgram(mProgram);
+        // get handle to vertex shader's vPosition member
+        mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+        // Enable a handle to the triangle vertices
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+        // Prepare the triangle coordinate data
+        GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX,
+                GLES20.GL_FLOAT, false,
+                vertexStride, vertexBuffer);
+        // get handle to fragment shader's vColor member
+        mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+        // Set color for drawing the triangle
+        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+        // Draw the triangle
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+        // Disable vertex array
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
+    }
+
+
 }
