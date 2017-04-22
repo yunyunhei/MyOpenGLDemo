7e749368617605ed0690f55c7f834a426871e177
yunyunhei <710617311@qq.com>
2017-04-22 19:40:21 +0800

    OpenGL的基础认识练习五，图形旋转

--- app/src/main/java/cn/yunyunhei/wuhang/myopengldemo/test1/MyGLRenderer.java
+++ app/src/main/java/cn/yunyunhei/wuhang/myopengldemo/test1/MyGLRenderer.java
@@ -3,6 +3,7 @@ package cn.yunyunhei.wuhang.myopengldemo.test1;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
+import android.os.SystemClock;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -46,8 +47,12 @@ public class MyGLRenderer implements GLSurfaceView.Renderer{
 
     }
 
+    private float[] mRotationMatrix = new float[16];
+
     @Override
     public void onDrawFrame(GL10 gl) {
+        float[] scratch = new float[16];
+
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
@@ -55,8 +60,19 @@ public class MyGLRenderer implements GLSurfaceView.Renderer{
         Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
         // Calculate the projection and view transformation
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+
+        // Create a rotation transformation for the triangle
+        long time = SystemClock.uptimeMillis() % 4000L;
+        float angle = 0.090f * ((int) time);
+        Matrix.setRotateM(mRotationMatrix, 0, angle, 0, 0, -1.0f);
+
+        // Combine the rotation matrix with the projection and camera view
+        // Note that the mMVPMatrix factor *must be first* in order
+        // for the matrix multiplication product to be correct.
+        Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
+
         // Draw shape
-        mTriangle.draw(mMVPMatrix);
+        mTriangle.draw(scratch);
     }
 
 
