602777f3f5d53e614bfeec098fdd36dc4b9751f8
yunyunhei <710617311@qq.com>
2017-04-22 19:43:56 +0800

    OpenGL的基础认识练习六，响应触摸事件使图形旋转

--- app/src/main/java/cn/yunyunhei/wuhang/myopengldemo/test1/MyGLRenderer.java
+++ app/src/main/java/cn/yunyunhei/wuhang/myopengldemo/test1/MyGLRenderer.java
@@ -3,10 +3,11 @@ package cn.yunyunhei.wuhang.myopengldemo.test1;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
-import android.os.SystemClock;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
+
+import static android.R.attr.angle;
 
 /**
  * Created by WuHang on 2017/4/22 0022.
@@ -62,9 +63,9 @@ public class MyGLRenderer implements GLSurfaceView.Renderer{
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
 
         // Create a rotation transformation for the triangle
-        long time = SystemClock.uptimeMillis() % 4000L;
-        float angle = 0.090f * ((int) time);
-        Matrix.setRotateM(mRotationMatrix, 0, angle, 0, 0, -1.0f);
+//        long time = SystemClock.uptimeMillis() % 4000L;
+//        float angle = 0.090f * ((int) time);
+        Matrix.setRotateM(mRotationMatrix, 0, mAngle, 0, 0, -1.0f);
 
         // Combine the rotation matrix with the projection and camera view
         // Note that the mMVPMatrix factor *must be first* in order
@@ -85,4 +86,13 @@ public class MyGLRenderer implements GLSurfaceView.Renderer{
         GLES20.glCompileShader(shader);
         return shader;
     }
+
+    public volatile float mAngle;
+    public float getAngle() {
+        return mAngle;
+    }
+    public void setAngle(float angle) {
+        mAngle = angle;
+    }
+
 }
--- app/src/main/java/cn/yunyunhei/wuhang/myopengldemo/test1/MyGLSurfaceView.java
+++ app/src/main/java/cn/yunyunhei/wuhang/myopengldemo/test1/MyGLSurfaceView.java
@@ -2,6 +2,7 @@ package cn.yunyunhei.wuhang.myopengldemo.test1;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.view.MotionEvent;
 
 /**
  * Created by WuHang on 2017/4/22 0022.
@@ -22,6 +23,38 @@ public class MyGLSurfaceView extends GLSurfaceView{
         setRenderer(mRenderer);
 
         // Render the view only when there is a change in the drawing data
-//        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+
+    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+    private float mPreviousX;
+    private float mPreviousY;
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        // MotionEvent reports input details from the touch screen
+        // and other input controls. In this case, you are only
+        // interested in events where the touch position changed.
+        float x = e.getX();
+        float y = e.getY();
+        switch (e.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                float dx = x - mPreviousX;
+                float dy = y - mPreviousY;
+                // reverse direction of rotation above the mid-line
+                if (y > getHeight() / 2) {
+                    dx = dx * -1 ;
+                }
+                // reverse direction of rotation to left of the mid-line
+                if (x < getWidth() / 2) {
+                    dy = dy * -1 ;
+                }
+                mRenderer.setAngle(
+                        mRenderer.getAngle() +
+                                ((dx + dy) * TOUCH_SCALE_FACTOR));
+                requestRender();
+        }
+        mPreviousX = x;
+        mPreviousY = y;
+        return true;
     }
 }
